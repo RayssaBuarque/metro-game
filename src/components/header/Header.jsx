d7bb5e08fd3efc6d046ea81990a8d6c484c39170
rayssabuarque <brayssa17@gmail.com>
2024-12-28 12:03:13 -0300

    update: stations & darkmode

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,38 @@
 import styles from './Header.module.css';
+import { useState, useEffect } from 'react';
 
 // ICONS
-import Info from '../../assets/infoSquare.svg';
+import Info from '../../assets/infoSquare.svg?react';
+import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md';
 
-const Header = () => {
+const Header = ({ mode, setMode }) => {    
+      const toggleMode = () => {
+        setMode((prevMode) => (prevMode === 'dark' ? 'light' : 'dark'));
+      };
+
     return (
         <div className={styles.container}>
+            {/* Título do jogo */}
             <h1>metro</h1>
-            {/* <Info style={{ fill: 'red' }} /> */}
-            <div className={styles.icon}></div>
+            
+            <div className={styles.extra}>
+                {/* Botão de dark/light mode */}
+                <div className={`${styles.toggleButton} ${mode === 'dark' ? styles.dark : styles.light}`}
+                     onClick={toggleMode} >
+                    <div className={styles.circle}>
+                        {mode === 'light' ? (
+                            <MdOutlineDarkMode size={18} color="#DBA159" />
+                        ) : (
+                          <MdOutlineLightMode size={18} color="#494949" />
+                        )}
+                    </div>
+                </div>
+
+                {/* Botão informativo*/}
+                <button>
+                    <Info style={{ color: 'var(--delicate_icon)', width: '25px', height: '25px' }} />
+                </button>
+            </div>
         </div>
     );
    };
--- src/components/station/Station.jsx
+++ src/components/station/Station.jsx
@@ -1,9 +1,22 @@
 import styles from './Station.module.css';
 
-const Station = ({name}) => {
+const Station = ({name, line, left, right}) => {
     return (
         <div className={styles.container}>
-            <div className={styles.dot}></div>
+
+            <div className={styles.station}>
+                <div 
+                    className={styles.left}
+                    style={{ backgroundColor: `var(--linha_${(left != null)? line : "transparent"})` }} ></div>
+                <div
+                    className={styles.dot}
+                    style={{ backgroundColor: `var(--linha_${line})` }} ></div>
+
+                <div
+                    className={styles.right}
+                    style={{ backgroundColor: `var(--linha_${(right != null)? line : "transparent"})` }} ></div>
+            </div>
+
             <label> {name} </label>
         </div>
     );
